134bd2bc6d7581f408753d6f810f9ae3b9e4c60f
Alexander Grist-Hucker <admin@revotech.org>
2015-08-26 01:15:55 +0100

    Changed the example column names.

--- example.lua
+++ example.lua
@@ -6,10 +6,10 @@ hook.Add("DatabaseConnected", "example.DatabaseConnected", function()
 
 	--[[ Create the "example" table if it does not exist. --]]
 	local queryObj = mysql:Create("example");
-		queryObj:Create("_Key", "int(11) NOT NULL AUTO_INCREMENT");
-		queryObj:Create("_Name", "varchar(255) NOT NULL");
-		queryObj:Create("_SteamID", "varchar(25) NOT NULL");
-		queryObj:PrimaryKey("_Key");
+		queryObj:Create("id", "int(11) NOT NULL AUTO_INCREMENT");
+		queryObj:Create("name", "varchar(255) NOT NULL");
+		queryObj:Create("steam_id", "varchar(25) NOT NULL");
+		queryObj:PrimaryKey("id");
 	queryObj:Execute();
 
 end);
@@ -18,22 +18,22 @@ hook.Add("PlayerInitialSpawn", "example.PlayerInitialSpawn", function(player)
 
 	--[[ Select the player's database entry by their steam id. --]]
 	local queryObj = mysql:Select("example");
-		queryObj:Where("_SteamID", player:SteamID());
+		queryObj:Where("steam_id", player:SteamID());
 		queryObj:Callback(function(result, status, lastID)
 			if (type(result) == "table" and #result > 0) then
 
 				--[[ Update the player's name in the database if it exists. --]]
 				local updateObj = mysql:Update("example");
-					updateObj:Update("_Name", player:Name());
-					updateObj:Where("_SteamID", player:SteamID());
+					updateObj:Update("name", player:Name());
+					updateObj:Where("steam_id", player:SteamID());
 				updateObj:Execute();
 
 			else
 
 				--[[ Insert the player's information into the example table. --]]
 				local insertObj = mysql:Insert("example");
-					insertObj:Insert("_Name", player:Name());
-					insertObj:Insert("_SteamID", player:SteamID());
+					insertObj:Insert("name", player:Name());
+					insertObj:Insert("steam_id", player:SteamID());
 					insertObj:Callback(function(result, status, lastID)
 						print(string.format("Added \"%\" to the example table", player:Name()));
 					end);
@@ -50,8 +50,8 @@ timer.Create("example.SaveData", 60, 0, function()
 
 		--[[ Queue and update to the player's name in the database (would usually be for data saving). --]]
 		local updateObj = mysql:Update("example");
-			updateObj:Update("_Name", v:Name());
-			updateObj:Where("_SteamID", v:SteamID());
+			updateObj:Update("name", v:Name());
+			updateObj:Where("steam_id", v:SteamID());
 		updateObj:Execute(true);
 		--[[ Passing true to Execute will queue the query. ]]--
 
